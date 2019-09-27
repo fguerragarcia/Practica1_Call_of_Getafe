232d714a9f9b41416c79c25b947f4ec98b2ae7c0
fguerragarcia <guerrafer98@gmail.com>
2019-09-27 09:38:28 +0200

    Initial commit

---
+++ Assets/_GameObjects/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Revolver revolver;
+   
+   
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0)) {
+
+            Disparar();
+        }
+
+    }
+
+    private void Disparar() {
+
+        print("Shoot");
+        revolver.Disparar();    
+    }
+}
---
+++ Assets/_GameObjects/Script/Revolver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolver : MonoBehaviour
+{
+
+    [SerializeField] GameObject prefabBala;
+    [SerializeField] Transform transformPuntoDeDisparo;
+    [SerializeField] float fuerzaDisparo;
+
+    
+    public void Disparar() {
+
+        //Instanciar bala(prefab bala)
+        GameObject bala = Instantiate(prefabBala);
+
+        //Posicionar bala(punto de spawn)
+        bala.transform.position = transformPuntoDeDisparo.transform.position;
+        bala.transform.rotation = transformPuntoDeDisparo.transform.rotation;
+
+        //Impulsar bala(rigidbody de la bala)
+        bala.GetComponent<Rigidbody>().AddRelativeForce(0, 0, fuerzaDisparo);
+
+    }
+}
